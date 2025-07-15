9cc9a2e5bb351b50264cdab313e9db1676335263
Spencer Vaughn <svaughn57@gmail.com>
2025-07-14 21:32:42 -0500

    remove generated output files and resource definitions

--- out/res/resources.d
+++
@@ -1,4 +0,0 @@
-out/res/resources.o: res/resources.res \
-/Users/spencer/Projects/sgdk-skeleton/res/background1.png \
-/Users/spencer/Projects/sgdk-skeleton/res/foreground1.png \
-/Users/spencer/Projects/sgdk-skeleton/res/moon.bmp
