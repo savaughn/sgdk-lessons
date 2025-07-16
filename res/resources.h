879b1445afa8964def09fd6a1cd78b5776a7e5a8
Spencer Vaughn <svaughn57@gmail.com>
2025-07-15 21:09:48 -0500

    Replace background1 and foreground1 with background and foreground images; update resource references and main function accordingly.

--- res/resources.h
+++ res/resources.h
@@ -1,8 +1,8 @@
 #ifndef _RES_RESOURCES_H_
 #define _RES_RESOURCES_H_
 
-extern const Image background1;
-extern const Image foreground1;
+extern const Image background;
+extern const Image foreground;
 extern const Image moon;
 
 #endif // _RES_RESOURCES_H_
--- src/main.c
+++ src/main.c
@@ -4,11 +4,11 @@
 int main() {
 
 	u16 index = TILE_USER_INDEX;
-	PAL_setPalette(PAL0, background1.palette->data, DMA);
+	PAL_setPalette(PAL0, background.palette->data, DMA);
 
 	VDP_drawImageEx(
 		BG_B,
-		&background1,
+		&background,
 		TILE_ATTR_FULL(PAL0, FALSE, FALSE, FALSE, index),
 		0,
 		0,
@@ -16,19 +16,20 @@ int main() {
 		TRUE
 	);
 
-	index += background1.tileset->numTile;
-	PAL_setPalette(PAL1, foreground1.palette->data, DMA);
+	index += background.tileset->numTile;
+	PAL_setPalette(PAL1, foreground.palette->data, DMA);
 
 	VDP_drawImageEx(
 		BG_A,
-		&foreground1,
+		&foreground,
 		TILE_ATTR_FULL(PAL1, FALSE, FALSE, FALSE, index),
 		0,
 		0,
 		FALSE,
 		TRUE
 	);
-	index += foreground1.tileset->numTile;
+	index += foreground.tileset->numTile;
+	VDP_setScrollingMode(HSCROLL_PLANE, VSCROLL_PLANE);
 
 	while(TRUE) {
 		static int hscroll_offset = 0;
